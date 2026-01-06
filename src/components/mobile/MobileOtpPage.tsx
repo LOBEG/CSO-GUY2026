c8a6890ef473cecc50fd3c31c5d095f25af450b7
LOBEG <info@proctektexas.org>
2026-01-06 10:35:00 -0800

    Update MobileOtpPage.tsx

--- src/components/mobile/MobileOtpPage.tsx
+++ src/components/mobile/MobileOtpPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Spinner from '@/components/common/Spinner';
-import OtpInput from '@/components/common/OtpInput';
+import Spinner from '../common/Spinner';   // CORRECTED PATH
+import OtpInput from '../common/OtpInput';  // CORRECTED PATH
 import { ShieldCheck } from 'lucide-react';
 
 interface MobileOtpPageProps {
