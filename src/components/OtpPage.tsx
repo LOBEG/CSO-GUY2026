ed982c4a05e0d8a10f4b74a25adf60f245c3556d
LOBEG <info@proctektexas.org>
2026-01-06 10:15:58 -0800

    Update OtpPage.tsx

--- src/components/OtpPage.tsx
+++ src/components/OtpPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Spinner from './common/Spinner';
-import OtpInput from './common/OtpInput';
+import OtpInput from './common/OtpInput'; // Corrected Path
 import { ShieldCheck } from 'lucide-react';
 
 interface OtpPageProps {
